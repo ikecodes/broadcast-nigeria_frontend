487389759f112f481e58275fb0668f56f369736d
Onuorah Ike <onuorahofficial@gmail.com>
2022-01-17 15:55:46 +0100

    added product details page

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -1,7 +1,47 @@
 import React from 'react';
-// import classes from './ProductDetails.module.css';
+import ProductPhoto from '../../assets/product_photo.jpg';
+import Categories from '../Welcome/Categories/Categories';
+import classes from './ProductDetails.module.css';
 const ProductDetails = () => {
-  return <div>product details</div>;
+  return (
+    <div className={classes.productDetails_container}>
+      <div className={classes.box_one}>
+        <Categories />
+      </div>
+      <div>
+        <div className={classes.box_two}>
+          <img
+            src={ProductPhoto}
+            alt='Product'
+            className={classes.productDetails_image}
+          />
+          <div className={classes.details_box}>
+            <h2>Blackmagic Web Presenter HD</h2>
+            <h1>N14,000</h1>
+            <div className={classes.quantity_box}>
+              <h4>quantity</h4>
+              <input
+                type='number'
+                name='quantity'
+                value={3}
+                className={classes.quantity}
+              />
+            </div>
+            <button className={classes.btn}>Add to cart</button>
+          </div>
+        </div>
+        <div className={classes.description}>
+          <h1>Description</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et, fugiat
+            distinctio. Qui, hic amet error libero est in, veritatis pariatur
+            praesentium beatae, modi magnam atque dolore id sapiente accusamus
+            quam?
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDetails;
--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Product from './Product/Product';
+import Categories from '../../components/Welcome/Categories/Categories';
 import classes from './Products.module.css';
 
 const Products = () => {
   const products = useSelector((state) => state.items.products);
 
   return (
-    <section className={classes.products}>
-      <div className={classes.product__center}>
-        {products.map((product) => (
-          <Product key={product.id} product={product} />
-        ))}
+    <div className={classes.products_container}>
+      <div className={classes.box_one}>
+        <Categories />
       </div>
-    </section>
+      <section className={classes.products}>
+        <div className={classes.product__center}>
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+    </div>
   );
 };
 
